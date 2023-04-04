939ffc8c6ad332ea4fd7af7db98e667d17513358
Intergalactic Igor <malte.baumann+bot@img.ly>
2023-04-04 14:53:24 +0000

    Update examples for v1.11.0-preview.1

--- engine-guides-create-scene-from-image-url/CreateSceneFromImageURL.kt
+++ engine-guides-create-scene-from-image-url/CreateSceneFromImageURL.kt
@@ -7,20 +7,20 @@ fun createSceneFromImageURL() = CoroutineScope(Dispatchers.Main).launch {
     engine.start()
     engine.bindOffscreen(width = 100, height = 100)
 
-    // highlight-initialImageURL
+    // highlight-createFromImage
     val imageRemoteUri = Uri.parse("https://img.ly/static/ubq_samples/sample_4.jpg")
     val scene = engine.scene.createFromImage(imageRemoteUri)
-    // highlight-initialImageURL
+    // highlight-createFromImage
 
-    // highlight-find-image
-    // Find the automatically added image element in the scene.
-    val image = engine.block.findByType(DesignBlockType.IMAGE).first()
-    // highlight-find-image
+    // highlight-findByType
+    // Find the automatically added image block in the scene.
+    val block = engine.block.findByType(DesignBlockType.IMAGE).first()
+    // highlight-findByType
 
-    // highlight-set-opacity
+    // highlight-setOpacity
     // Change its opacity.
-    engine.block.setOpacity(image, value = 0.5F)
-    // highlight-set-opacity
+    engine.block.setOpacity(block, value = 0.5F)
+    // highlight-setOpacity
 
     engine.stop()
 }
