3924c01c9af86285af55769caf260a9de618a86a
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-08-23 09:12:15 +0200

    Update examples for v1.33.0

--- editor-base/src/main/java/ly/img/editor/base/dock/options/shapeoptions/ShapeOptionsUiState.kt
+++ editor-base/src/main/java/ly/img/editor/base/dock/options/shapeoptions/ShapeOptionsUiState.kt
@@ -1,10 +1,22 @@
 package ly.img.editor.base.dock.options.shapeoptions
 
+import ly.img.editor.base.engine.getSmallerSide
 import ly.img.engine.DesignBlock
 import ly.img.engine.Engine
 import ly.img.engine.ShapeType
 
 sealed interface ShapeOptionsUiState
+
+interface RoundableShapeUiState {
+    val cornerRadius: Float
+}
+
+data class RectShapeUiState(
+    val cornerRadiusTopLeft: Float,
+    val cornerRadiusTopRight: Float,
+    val cornerRadiusBottomLeft: Float,
+    val cornerRadiusBottomRight: Float,
+) : ShapeOptionsUiState
 
 data class StarShapeOptionsUiState(
     val points: Float,
@@ -13,7 +25,9 @@ data class StarShapeOptionsUiState(
 
 data class PolygonShapeOptionsUiState(
     val sides: Float,
-) : ShapeOptionsUiState
+    override val cornerRadius: Float,
+) : ShapeOptionsUiState,
+    RoundableShapeUiState
 
 data class LineShapeOptionsUiState(
     val width: Float,
@@ -34,6 +48,11 @@ internal fun createShapeOptionsUiState(
         ShapeType.Polygon ->
             PolygonShapeOptionsUiState(
                 sides = engine.block.getInt(shape, "shape/polygon/sides").toFloat(),
+                cornerRadius =
+                    engine.block.getFloat(
+                        shape,
+                        "shape/polygon/cornerRadius",
+                    ) / (engine.block.getSmallerSide(designBlock) / 2f),
             )
 
         ShapeType.Line ->
@@ -41,8 +60,18 @@ internal fun createShapeOptionsUiState(
                 width = engine.block.getFrameHeight(designBlock),
             )
 
+        ShapeType.Rect -> {
+            val maxRadius = engine.block.getSmallerSide(designBlock) / 2f
+            RectShapeUiState(
+                cornerRadiusTopLeft = engine.block.getFloat(shape, "shape/rect/cornerRadiusTL") / maxRadius,
+                cornerRadiusTopRight = engine.block.getFloat(shape, "shape/rect/cornerRadiusTR") / maxRadius,
+                cornerRadiusBottomLeft = engine.block.getFloat(shape, "shape/rect/cornerRadiusBL") / maxRadius,
+                cornerRadiusBottomRight = engine.block.getFloat(shape, "shape/rect/cornerRadiusBR") / maxRadius,
+            )
+        }
+
         else -> throw IllegalArgumentException(
-            "Options are only supported for star, polygon, and line shape types.",
+            "Options are only supported for star, polygon, line shape types and rects.",
         )
     }
 }
