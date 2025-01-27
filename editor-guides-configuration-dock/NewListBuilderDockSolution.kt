925e538dfd235f27abd5c734e96dbf99b2510721
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-01-27 10:09:09 +0100

    Update examples for v1.44.0-rc.0

--- editor-guides-configuration-dock/NewListBuilderDockSolution.kt
+++ editor-guides-configuration-dock/NewListBuilderDockSolution.kt
@@ -1,10 +1,8 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import ly.img.editor.DesignEditor
 import ly.img.editor.EditorConfiguration
 import ly.img.editor.EngineConfiguration
-import ly.img.editor.core.LocalEditorScope
 import ly.img.editor.core.component.Dock
 import ly.img.editor.core.component.EditorComponentId
 import ly.img.editor.core.component.rememberElementsLibrary
@@ -31,16 +29,11 @@ fun NewListBuilderDockSolution(navController: NavHostController) {
                     listBuilder =
                         Dock.ListBuilder.remember {
                             add {
-                                val buttonScope =
-                                    LocalEditorScope.current.run {
-                                        remember(this) { Dock.ButtonScope(parentScope = this) }
-                                    }
                                 Dock.Button.remember(
                                     id = EditorComponentId("my.package.dock.button.custom"),
                                     vectorIcon = null,
                                     text = { "Custom Button" },
                                     onClick = {},
-                                    scope = buttonScope,
                                 )
                             }
                             add { Dock.Button.rememberSystemGallery() }
