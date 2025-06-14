2dc83f9f8df3783a7f8a79a27322c90a895fb4f9
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-15 00:34:30 +0200

    Update examples for v1.54.0-rc.0

--- editor-guides-configuration-canvas-menu/NewListBuilderCanvasMenuSolution.kt
+++ editor-guides-configuration-canvas-menu/NewListBuilderCanvasMenuSolution.kt
@@ -6,7 +6,9 @@ import ly.img.editor.EngineConfiguration
 import ly.img.editor.core.component.CanvasMenu
 import ly.img.editor.core.component.EditorComponentId
 import ly.img.editor.core.component.rememberBringForward
+import ly.img.editor.core.component.rememberDelete
 import ly.img.editor.core.component.rememberDuplicate
+import ly.img.editor.core.component.rememberSelectGroup
 import ly.img.editor.core.component.rememberSendBackward
 import ly.img.editor.rememberForDesign
 
@@ -30,10 +32,13 @@ fun NewListBuilderCanvasMenuSolution(navController: NavHostController) {
                             text = { "Custom Button" },
                         )
                     }
+                    add { CanvasMenu.Button.rememberSelectGroup() }
+                    add { CanvasMenu.Divider.remember(visible = { editorContext.isSelectionInGroup }) }
                     add { CanvasMenu.Button.rememberSendBackward() }
                     add { CanvasMenu.Button.rememberBringForward() }
                     add { CanvasMenu.Divider.remember(visible = { editorContext.canSelectionMove }) }
                     add { CanvasMenu.Button.rememberDuplicate() }
+                    add { CanvasMenu.Button.rememberDelete() }
                 },
                 // highlight-newListBuilder
             )
