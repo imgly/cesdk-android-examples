b17cb374fca63a0197be9c13b64e48553400396f
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-06-27 16:31:11 +0200

    Update examples for v1.55.0-rc.0

--- editor-guides-configuration-canvas-menu/CanvasMenuListBuilders.kt
+++ editor-guides-configuration-canvas-menu/CanvasMenuListBuilders.kt
@@ -3,11 +3,14 @@ import ly.img.editor.core.component.CanvasMenu
 import ly.img.editor.core.component.rememberBringForward
 import ly.img.editor.core.component.rememberDelete
 import ly.img.editor.core.component.rememberDuplicate
+import ly.img.editor.core.component.rememberSelectGroup
 import ly.img.editor.core.component.rememberSendBackward
 
 // highlight-listBuilders
 @Composable
 fun CanvasMenu.ListBuilder.remember() = CanvasMenu.ListBuilder.remember {
+    add { CanvasMenu.Button.rememberSelectGroup() }
+    add { CanvasMenu.Divider.remember(visible = { editorContext.isSelectionInGroup }) }
     add { CanvasMenu.Button.rememberBringForward() }
     add { CanvasMenu.Button.rememberSendBackward() }
     add { CanvasMenu.Divider.remember(visible = { editorContext.canSelectionMove }) }
