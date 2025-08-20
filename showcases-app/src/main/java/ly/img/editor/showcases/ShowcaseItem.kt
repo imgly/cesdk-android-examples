a8f854ff900afdefa80fdba87240c08b0978ff1e
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-08-20 06:11:39 +0200

    Update showcases for v1.58.0

---
+++ showcases-app/src/main/java/ly/img/editor/showcases/ShowcaseItem.kt
@@ -0,0 +1,55 @@
+package ly.img.editor.showcases
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+sealed class ShowcaseItem(
+    open val actionScene: String? = null,
+    open val actionScreen: Screen? = null,
+    open val span: Int,
+    val key: String,
+) {
+    data class Header(
+        @StringRes val title: Int,
+        override val span: Int,
+        val items: List<ShowcaseItem> = emptyList(),
+    ) : ShowcaseItem(
+            actionScene = null,
+            actionScreen = null,
+            span = span,
+            key = "item.header.$title",
+        )
+
+    data class Content(
+        @DrawableRes val thumbnailRes: Int,
+        override val actionScene: String,
+        override val actionScreen: Screen,
+        val thumbnailAspectRatio: Float = 1F,
+    ) : ShowcaseItem(
+            actionScene = actionScene,
+            actionScreen = actionScreen,
+            span = 1,
+            key = "item.content.$actionScene",
+        )
+
+    class CarouselContent(
+        @DrawableRes val iconRes: Int,
+        @StringRes val label: Int,
+        @StringRes val sublabel: Int?,
+        val hasDotLine: Boolean = false,
+        val clickAction: ClickAction = ClickAction.OPEN_SCENE,
+        override val actionScene: String?,
+        override val actionScreen: Screen,
+    ) : ShowcaseItem(
+            actionScene = actionScene,
+            actionScreen = actionScreen,
+            span = 1,
+            key = "item.content.$actionScene",
+        ) {
+        enum class ClickAction {
+            OPEN_SCENE,
+            PICK_SCENE,
+            PICK_IMAGE,
+        }
+    }
+}
